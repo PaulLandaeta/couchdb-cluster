1fcf91849a58ccc065e2076d3297cb31447389c4
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-04-25 21:33:48 -0400

    add init function

--- src/core.py
+++ src/core.py
@@ -1,8 +1,9 @@
 from clients import store_clients
 from movies import store_movies
 
+def run():
+    store_clients()
+    store_movies()
 
-#TODO: create init
-store_clients()
-
-store_movies()
+if __name__ == '__main__':
+    run() 
