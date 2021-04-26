273de3354ebda4ece32272dd680fda6faed9dee1
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-04-25 21:24:06 -0400

    add json

--- src/core.py
+++ src/core.py
@@ -1,3 +1,8 @@
 from clients import store_clients
+from movies import store_movies
 
+
+#TODO: create init
 store_clients()
+
+store_movies()
---
+++ src/movies.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from requests.models import HTTPBasicAuth 
+
+
+URL = 'http://127.0.0.1:5984/club_de_videos/'
+USERNAME = 'admin'
+PASSWORD = '123456'
+
+
+def read_movies():
+    with open('data/movies.json') as f:
+        movies = json.load(f)
+    return movies
+
+
+def store_movies():
+    movies = read_movies()
+    for movie in movies:
+        response = requests.post(
+            URL, json = movie, auth = HTTPBasicAuth(USERNAME, PASSWORD))
+        print(response.content)
